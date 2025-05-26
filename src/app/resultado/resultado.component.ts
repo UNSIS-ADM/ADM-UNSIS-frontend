87f8569f8055e25516732f5850b9a44b8b3d66a3
carlosaP131 <carlos.cantrax303@gmail.com>
2025-05-26 04:49:30 -0600

    agregar componente de navegación en la página de resultados

--- src/app/resultado/resultado.component.ts
+++ src/app/resultado/resultado.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
 
 @Component({
   selector: 'app-resultado',
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './resultado.component.html',
   styleUrl: './resultado.component.css'
 })
