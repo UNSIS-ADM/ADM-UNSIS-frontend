e68944222bf3075a993dc99de216083f959100e3
Cantrax <carlos.cantrax303@gmail.com>
2025-06-03 12:11:01 -0600

    tailwild

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,12 +3,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatLabel],
+  imports: [RouterModule,MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatLabel],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
