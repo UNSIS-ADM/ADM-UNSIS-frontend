7ef53582184140041a5619a6dde36c3536aa9044
Cantrax <carlos.cantrax303@gmail.com>
2025-06-04 09:47:50 -0600

    creacion footer

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'unsis': {
+          primary: '#6a1b1b',
+          secondary: '#8b2424',
+        },
+      },
+    },
+  },
+  // ...resto de la configuración
+}
