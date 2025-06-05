f79cfc602937e8130a27f4d607d23afb05cb6d83
Cantrax <carlos.cantrax303@gmail.com>
2025-06-05 12:43:59 -0600

    Errores corregidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,10 @@ import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FooterComponent } from "./footer/footer.component";
-import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatMenuModule, MatSidenavModule, MatDividerModule, MatButtonModule, FooterComponent, NavComponent],
+  imports: [RouterOutlet, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatMenuModule, MatSidenavModule, MatDividerModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
