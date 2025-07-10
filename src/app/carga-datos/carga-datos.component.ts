3c4f3b19740a015cdc5ed2c73d9c4ff7a8eacfbd
Sainos <qa.kadasoftware@gmail.com>
2025-07-10 12:05:11 -0600

    feat: agregar carga masiva de aspirantes desde archivo Excel

--- src/app/carga-datos/carga-datos.component.ts
+++ src/app/carga-datos/carga-datos.component.ts
@@ -1,4 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+
+// src/app/components/alumnos/alumnos.component.ts
+import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ExcelService, ExcelUploadResponse} from '../services/excel.service';
+import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-alumnos',
+  standalone: true,
+  imports: [HttpClientModule, NavComponent, FooterComponent, NgClass],
+  templateUrl: './carga-datos.component.html',
+  styleUrls: ['./carga-datos.component.css'],
+})
+export class AlumnosComponent {
+  selectedFile: File | null = null;
+  uploadResult: ExcelUploadResponse | null = null;
+  token = localStorage.getItem('token') || ''; // o como guardes tu token
+
+  constructor(private excelService: ExcelService) {}
+
+  onFileSelected(evt: Event) {
+    const input = evt.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  uploadExcel() {
+    if (!this.selectedFile) {
+      alert('Selecciona primero un archivo .xlsx');
+      return;
+    }
+    this.excelService
+      .uploadApplicants(this.selectedFile, this.token)
+      .subscribe({
+        next: (res) => (this.uploadResult = res),
+        error: (err) => {
+          console.error(err);
+          alert('Error al subir el Excel');
+        },
+      });
+  }
+}
+/*import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -27,3 +73,4 @@ export class CargaDatosComponent implements OnInit {
     });
   }
 }
+*/
