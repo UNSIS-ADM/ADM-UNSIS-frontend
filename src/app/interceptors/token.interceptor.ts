3f3b056fe5176d544ffd40eda49836686338d200
Sainos <qa.kadasoftware@gmail.com>
2025-10-13 10:02:35 -0600

    fix: se agrego el cambio para que los administrativos puedan descargar los formatos xlsx de ejemplos y el campo de fecha de examen en la tabla de carga de datos.

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,13 +1,10 @@
-// src/app/interceptors/token.interceptor.ts
-
-
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   let token = localStorage.getItem('token') || '';
   if (!token) return next(req);
-  console.debug('authInterceptor token found?', !!token, req.url);
 
+  console.debug('authInterceptor token found?', !!token, req.url);
 
   const headerValue = token.startsWith('Bearer ') ? token : `Bearer ${token}`;
   const clonedReq = req.clone({
@@ -15,3 +12,4 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   });
   return next(clonedReq);
 };
+
